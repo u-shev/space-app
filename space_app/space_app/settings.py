4fec9f671d463910402e58cd09e9558645797f11
u-shev <sheveleva.jul@gmail.com>
2024-01-22 12:23:43 +0300

    poetry added psycopg2-binary
    changed CI
    
    changed CI
    
    changed CI
    
    changed settings
    
    CI
    
    CI fix

--- space_app/space_app/settings.py
+++ space_app/space_app/settings.py
@@ -107,6 +107,17 @@ DATABASES = {
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
     }
 }
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
