0815e017b3ae2de217f1379e70a8b2e9eb68875d
u-shev <sheveleva.jul@gmail.com>
2024-02-14 20:03:37 +0300

    handling nasa api errors

--- space_app/space_app/helpers.py
+++ space_app/space_app/helpers.py
@@ -25,16 +25,21 @@ async def get_data_from_api():
     async with aiohttp.ClientSession() as session:
         response = await session.get(
             f'https://api.nasa.gov/planetary/apod?api_key={nasa_api}')
-        nasa_json = await response.json()
-        nasa_info = nasa_json['explanation']
-        if nasa_json['media_type'] == "image":
-            nasa_picture = nasa_json['url']
-            nasa_video = None
-        elif nasa_json['media_type'] == "video":
-            nasa_video = nasa_json['url']
-            nasa_picture = None
+        if response.status == 200:
+            nasa_json = await response.json()
+            nasa_info = nasa_json['explanation']
+            if nasa_json['media_type'] == "image":
+                nasa_picture = nasa_json['url']
+                nasa_video = None
+            elif nasa_json['media_type'] == "video":
+                nasa_video = nasa_json['url']
+                nasa_picture = None
+            else:
+                nasa_video = None
+                nasa_picture = None
         else:
+            nasa_info = "Some problems with Nasa API occured, so you see this picture"
             nasa_video = None
-            nasa_picture = None
+            nasa_picture = "https://apod.nasa.gov/apod/image/2402/RosetteCone_Bernard_5398.jpg"
         context = nasa_info, nasa_picture, nasa_video
         return context
